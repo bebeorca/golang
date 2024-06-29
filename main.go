9fa79336bc372971e6bd893d3d87c45278af6e97
bebeorca <dimasrdyo@gmail.com>
2024-06-30 06:10:28 +0700

    Update End Points

--- main.go
+++ main.go
@@ -4,11 +4,23 @@ import (
 	"log"
 	"os"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
+
+type Response struct {
+    Message string `json:"message"`
+    Status  int    `json:"status"`
+}
+
 
 func main(){
 	
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+        AllowOrigins: "*", // Allow all origins
+        AllowHeaders: "Origin, Content-Type, Accept",
+    }))
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, Railway!")
@@ -18,11 +30,13 @@ func main(){
 		return c.SendString("Hello, ENV! " + os.Getenv("TEST_ENV"))
 	})
 
-	app.Get("/tjson", func(c *fiber.Ctx) error {
-		return c.Status(200).JSON(fiber.Map{
-		 "message": "Hello",
-		})
-	   })
+	app.Get("/json", func(c *fiber.Ctx) error {
+        response := Response{
+            Message: "Hello, World!",
+            Status:  200,
+        }
+        return c.JSON(response)
+    })
 
 	port := os.Getenv("PORT")
 
